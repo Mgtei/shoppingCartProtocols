3b3f89a4c706f118c6ceb106a61a73d2c0489a93
Marty Hernandez Avedon <martyavedon@gmail.com>
2017-01-30 11:45:48 -0500

    Initial Commit

---
+++ ShoppingCartProtocols/ShoppingCartItem.swift
@@ -0,0 +1,19 @@
+//
+//  ShoppingCartItem.swift
+//  ShoppingCartProtocols
+//
+//  Created by Marty Avedon on 1/30/17.
+//  Copyright © 2017 Marty Hernandez Avedon. All rights reserved.
+//
+
+import Foundation
+
+struct ShoppingCartItem {
+    let price: Double
+    let name:  String
+    let sku: String
+    let quantity: Int
+    let taxed: Bool
+    
+    
+}
---
+++ ShoppingCartProtocols/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ShoppingCartProtocols
+//
+//  Created by Marty Avedon on 1/30/17.
+//  Copyright © 2017 Marty Hernandez Avedon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
